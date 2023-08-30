ae3da463db6fb75748645a1fc3351c022b772e5c
Deepak Chaturvedi <deepakchaturvedifzd@gmail.com>
2023-08-30 14:58:24 +0530

    added initial files

---
+++ fireBaseConfig.js
@@ -0,0 +1,9 @@
+export default firebaseConfig = {
+  apiKey: "AIzaSyDFNg4s0eUFDO2RaZ4Yx2Ss3CraEYsNA0c",
+  authDomain: "collaboravoice.firebaseapp.com",
+  projectId: "collaboravoice",
+  storageBucket: "collaboravoice.appspot.com",
+  messagingSenderId: "611789700851",
+  appId: "1:611789700851:web:94b6643d68ab72887dc186",
+  measurementId: "G-NQ1YN3Y7WJ",
+};
